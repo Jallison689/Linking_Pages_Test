0a080d395ea9c30a76e170931cd6d8d2659dccd9
Jallison689 <Cyl0n54r3Bull5h1t>
2019-02-21 18:31:19 +0000

    temp

---
+++ app/src/main/java/com/example/android/myapplication/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.android.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button next = findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), MainActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+}
